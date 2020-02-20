b34c0a026391798891273ee6634cc93d197204a2
Harshita Garg <26150304+mercuryhg31@users.noreply.github.com>
2020-02-20 03:20:30 -0500

    back to javafx :/

--- src/app/App.java
+++ src/app/App.java
@@ -1,117 +1,47 @@
 package app;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
-import dict.Word;
-
-public class App {
-    public static void main(String[] args) {
-        
-    }
-}
-
-/* ///////////////////////////////////// NOTES ////////////////////////
-// SERIALIZATION (this words btw)
-List<Definition> defs = new ArrayList<Definition>();
-defs.add(new Definition("hi2", "hi3"));
-defs.add(new Definition("hi4", "hi5"));
-
-List<String> syns = new ArrayList<String>();
-syns.add("hi6");
-syns.add("hi7");
-
-List<String> ants = new ArrayList<String>();
-ants.add("hi8");
-ants.add("hi9");
-
-System.out.println(new Gson().toJson(new Word("hi", defs, syns, ants)));
-
-
-
-// SERIALIZING ARRAY OF WORDS (this also works)
-List<Definition> defs = new ArrayList<Definition>();
-defs.add(new Definition("hi2", "hi3"));
-defs.add(new Definition("hi4", "hi5"));
-
-List<String> syns = new ArrayList<String>();
-syns.add("hi6");
-syns.add("hi7");
-
-List<String> ants = new ArrayList<String>();
-ants.add("hi8");
-ants.add("hi9");
-
-List<Word> words = new ArrayList<Word>();
-words.add(new Word("hi00", defs, syns, ants));
-words.add(new Word("hi01", defs, syns, ants));
-words.add(new Word("hi02", defs, syns, ants));
-
-System.out.println(new Gson().toJson(words));
-
-//////////////////// pretty printing ////////////////
-Gson gson = new GsonBuilder().setPrettyPrinting().create(); <-- to keep it in tree structure
-
-*/
-
-
-
-
-/* //////////////////////////////////////////////// JAVA FX APP LEARNING ////////////////////////
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
 
 public class App extends Application {
 
     Stage window;
-    Scene one, two;
+    BorderPane setup = new BorderPane();
 
     @Override
     public void start(Stage stage) {
         window = stage;
 
-        Label l1 = new Label("Sup, y'all've arrived at the first scene");
-        Button b1 = new Button("Now go to the second scene");
-        b1.setOnAction(e -> window.setScene(two));
+        setLeftFrame();
+        setCenterFrame();
 
-        VBox layout1 = new VBox(20);
-        layout1.getChildren().addAll(l1, b1);
-        one = new Scene(layout1, 300, 300);
+        Scene main = new Scene(setup, 500, 500);
 
-        Label l2 = new Label("Okay, now go back nothing to see here");
-        Button b2 = new Button("Leave");
-        b2.setOnAction(e -> window.setScene(one));
-
-        // b2.setOnAction(new EventHandler<ActionEvent>() {
-        //     @Override
-        //     public void handle(ActionEvent event) {
-        //         window.setScene(one);
-        //     }
-        // });
-
-        VBox layout2 = new VBox(20);
-        layout2.getChildren().addAll(l2, b2);
-        two = new Scene(layout2, 600, 600);
-
-        window.setScene(one);
-        window.setTitle("Learning under construction");
+        window.setScene(main);
+        window.setTitle("Desktop Dictionary");
         window.show();
+    }
 
-        // String javaVersion = System.getProperty("java.version");
-        // String javafxVersion = System.getProperty("javafx.version");
-        // Label testVar = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        // testVar = new Label(Test.testString());
-        // Scene scene = new Scene(new StackPane(testVar), 640, 480);
-        // stage.setScene(scene);
-        // stage.show();
+    void setLeftFrame() {
+        StackPane left = new StackPane();
+
+        left.getChildren().add(new Label("hi left"));
+
+        setup.setLeft(left);
+    }
+
+    void setCenterFrame() {
+        StackPane center = new StackPane();
+        center.getChildren().add(new Label("hi center"));
+
+        setup.setCenter(center);
     }
 
     public static void main(String[] args) {
         launch(args);
-        Word[] words = Word.getWords();
-        for (Word word : words) {
-            System.out.println(word.getWord());
-        }
     }
 
 }
-
-*/
